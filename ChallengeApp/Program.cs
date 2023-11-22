3a8b17a7317886a4fbd98570a94a68429de930f6
DamianP56 <Damianpalac6@gmail.com>
2023-11-22 00:21:15 +0000

    Add project files.

---
+++ ChallengeApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using ChallengeApp;
+
+User User = new User("Damian", "Palec", 26);
+User user2 = new User("Rafał", "Patrzykalski", 29);
+User user3 = new User("Donek", "Kortok", 23);
+
+User.AddScore(3);
+User.AddScore(4);
+User.AddScore(8);
+
+user2.AddScore(1);
+user2.AddScore(8);
+user2.AddScore(9);
+
+user3.AddScore(10);
+user3.AddScore(7);
+user3.AddScore(6);
+
+List<User> users = new List<User>()
+
+{
+    User,user2,user3
+};
+
+int maxResult = -1;
+User? userWithmaxResult = null;
+
+foreach (var user in users)
+{
+    if (user.Result > maxResult)
+    {
+        userWithmaxResult = user;
+    }
+}
+
+Console.WriteLine(userWithmaxResult);
---
+++ ChallengeApp/User.cs
@@ -0,0 +1,36 @@
+﻿namespace ChallengeApp
+{
+    public class User
+    {
+
+        public List<int> score = new List<int>();
+        public string v1;
+        public string v2;
+
+        public User(string v1, string v2)
+        {
+            this.v1 = v1;
+            this.v2 = v2;
+        }
+
+        public User(string v1, string v2, int v)
+        {
+            this.v1 = v1;
+            this.v2 = v2;
+        }
+
+        public User(string name, string surname, string age)
+        {
+            this.Name = name;
+            this.Surname = surname;
+            this.Age = age;
+        }
+
+
+
+
+
+
+
+
+
