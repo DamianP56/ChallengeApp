45b2935b159b30720763fbf65974baac79229d15
DamianP56 <Damianpalac6@gmail.com>
2023-12-08 04:07:28 +0000

    czyste

--- ChallengeApp/Program.cs
+++ ChallengeApp/Program.cs
@@ -7,30 +7,6 @@ employee.AddGrade("Sebastian");
 employee.AddGrade(5);
 employee.AddGrade('8');
 
-var statistics1 = employee.GetStatisticsWithForEach();
-var statistics2 = employee.GetStatisticsWithFor();
-var statistics3 = employee.GetStatisticsWithDoWhile();
-var statistics4 = employee.GetStatisticsWithWile();
-
-Console.WriteLine("Wynik1 : ");
-Console.WriteLine($"środkowa ocena; {statistics1.Average:n2}");
-Console.WriteLine($"niska: {statistics1.Min}");
-Console.WriteLine($"bardzo duża: {statistics1.Max}");
-
-Console.WriteLine("Wynik2 : ");
-Console.WriteLine($"środkowa ocena; {statistics2.Average:n2}");
-Console.WriteLine($"niska: {statistics2.Min}");
-Console.WriteLine($"bardzo duża: {statistics2.Max}");
-
-Console.WriteLine("Wynik3 : ");
-Console.WriteLine($"środkowa ocena; {statistics3.Average:n2}");
-Console.WriteLine($"niska: {statistics3.Min}");
-Console.WriteLine($"bardzo duża: {statistics3.Max}");
-
-Console.WriteLine("Wynik4 : ");
-Console.WriteLine($"środkowa ocena; {statistics4.Average:n2}");
-Console.WriteLine($"niska: {statistics4.Min}");
-Console.WriteLine($"bardzo duża: {statistics4.Max}");
 
 
 
