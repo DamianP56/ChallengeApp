f42369bfac00b502a3ec1a998c62d3b1272a3342
DamianP56 <Damianpalac6@gmail.com>
2023-12-24 19:26:08 +0000

    day 14

---
+++ ChallengeApp/Person.cs
@@ -0,0 +1,29 @@
+﻿namespace ChallengeApp
+{
+    public abstract class Person 
+    {
+       
+        public Person(string name, string surname, int age, string sex) 
+        {
+            this.Name = name;
+            this.Surname = surname;
+            this.Sex = sex;
+            this.Age = age;
+        }
+
+        protected Person(string name, string surname)
+        {
+            Name = name;
+            Surname = surname;
+        }
+
+        public string Name { get; private set; }
+        public string Surname { get; private set; }
+        public string Sex { get; private set; }
+        public int Age { get; private set; }
+
+
+    }
+}
+
+
